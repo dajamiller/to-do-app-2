99865af078ffcb2fce2390e754966b75ea972092
Maryjane Watanabe <maryjanekw@gmail.com>
2025-11-25 13:40:16 -0800

    Task Manager class and branch added

---
+++ src/main/java/com/pluralsight/TaskManager.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+public class TaskManager {
+
+    private List<Task> tasks = new ArrayList<>();
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
+    public boolean markTaskDone(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).setDone(true);
+            return true;
+        }
+        return false;
+    }
+    public boolean deleteTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+}
